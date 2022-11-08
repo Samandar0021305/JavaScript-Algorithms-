a202847f559e1608cfe966f0e79420d47a229ac0
Samandar.log <TSamandardev@gmail.com>
2022-11-08 10:48:39 +0500

    init

--- js/main.js
+++ js/main.js
@@ -1,14 +1,46 @@
 
-function foo(n){
+// function foo(n){
 //   let summa =0;
 //   for(let i=0 ;i < n;i++){
 //     summa +=i
 //   }
 //   return summa
 
-return (n*(n-1)) / 2
-}
+// return (n*(n+1)) / 2
+// }
 
 
 
-console.log(foo(912341232));
+// console.log(foo(9));
+
+
+
+// const person  ={
+//     firstName:'Bruce',
+//     lastName:'Wayne'
+// }
+
+
+
+// let desc = Object.entries(person)
+
+// console.log(typeof desc);
+
+
+
+// fibonachi number
+
+
+
+// function fibonachi(n){
+//     const fib = [0,1]
+//    for(let i=2;i<n;i++){
+//     fib[i] = fib[i-1] + fib[i-2]
+//    }
+//    return fib
+// }
+
+// console.log(fibonachi(51));
+
+
+
